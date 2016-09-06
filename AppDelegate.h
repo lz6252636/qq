6e59ea2d11337085a027fd34bc91a4c6ba0a2e4c
lz6252636 <993277317@qq.com>
2016-09-06 21:41:18 +0800

    新增目录

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq
+//
+//  Created by liuzhao on 16/9/6.
+//  Copyright © 2016年 liuzhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq
+//
+//  Created by liuzhao on 16/9/6.
+//  Copyright © 2016年 liuzhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
